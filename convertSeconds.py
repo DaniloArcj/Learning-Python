6525d5a44ceca1aa2fe1f0bc0ebdd199bdf04c9a
Danilo Arcanjo <daniloarcj@gmail.com>
2022-06-07 20:08:59 -0300

    Add a loop that ends when 'exit' is typed

--- convertSeconds.py
+++ convertSeconds.py
@@ -1,8 +1,11 @@
-seconds = input("Type how many seconds: ")
-
-day = ((int(seconds)//60)//60)//24
-hrs = ((int(seconds)//60)//60)%24
-min = (int(seconds)//60)%60
-sec = int(seconds)%60
-
-print(seconds, "sec = ", day, "day", hrs, "hrs", min, "min", sec, "sec", "\n")
+while True :
+    seconds = input("Type how many seconds: ")
+    if seconds == "exit" :
+        break
+    else :
+        day = ((int(seconds)//60)//60)//24
+        hrs = ((int(seconds)//60)//60)%24
+        min = (int(seconds)//60)%60
+        sec = int(seconds)%60
+    
+        print(seconds, "sec = ", day, "day", hrs, "hrs", min, "min", sec, "sec", "\n")
